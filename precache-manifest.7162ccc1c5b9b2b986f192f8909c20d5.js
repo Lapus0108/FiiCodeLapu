3760220cfe410e9f9296e6de4cdf55c27bd38044
Mihnea <rmihneaadrian@yahoo.com>
2020-04-10 18:48:43 +0300

    Updates

---
+++ precache-manifest.7162ccc1c5b9b2b986f192f8909c20d5.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "217eaab040c172ab89221084e3e9b402",
+    "url": "/FiiCodeCodLapu/index.html"
+  },
+  {
+    "revision": "317e825522afe321dd73",
+    "url": "/FiiCodeCodLapu/static/css/2.8d78c2cf.chunk.css"
+  },
+  {
+    "revision": "be7b01f82c1066fabd98",
+    "url": "/FiiCodeCodLapu/static/css/main.e9056b9d.chunk.css"
+  },
+  {
+    "revision": "317e825522afe321dd73",
+    "url": "/FiiCodeCodLapu/static/js/2.58f94286.chunk.js"
+  },
+  {
+    "revision": "3c56bc67e1bc8d824c3031c43b8a138c",
+    "url": "/FiiCodeCodLapu/static/js/2.58f94286.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be7b01f82c1066fabd98",
+    "url": "/FiiCodeCodLapu/static/js/main.a9dc2840.chunk.js"
+  },
+  {
+    "revision": "43f0232903cf146592dd",
+    "url": "/FiiCodeCodLapu/static/js/runtime-main.12ba995b.js"
+  },
+  {
+    "revision": "771a9da6581e4cc1eb0eda8704042e70",
+    "url": "/FiiCodeCodLapu/static/media/About.771a9da6.svg"
+  },
+  {
+    "revision": "81b00387e97863532bfc5b510ccab51e",
+    "url": "/FiiCodeCodLapu/static/media/CreateAccount.81b00387.svg"
+  },
+  {
+    "revision": "5a4191db91641f0e31bba99bde3c83b9",
+    "url": "/FiiCodeCodLapu/static/media/Franchise.5a4191db.ttf"
+  },
+  {
+    "revision": "21cda9fd66db118beeaab85590663a60",
+    "url": "/FiiCodeCodLapu/static/media/FranchiseFilled.21cda9fd.ttf"
+  },
+  {
+    "revision": "bb0a524de9ffbf2299d8023f7938c089",
+    "url": "/FiiCodeCodLapu/static/media/Login.bb0a524d.svg"
+  },
+  {
+    "revision": "83f272e67867ed13406199ed63cd7229",
+    "url": "/FiiCodeCodLapu/static/media/Logout.83f272e6.svg"
+  },
+  {
+    "revision": "79d4cd7111445919c9a290eac2e70de0",
+    "url": "/FiiCodeCodLapu/static/media/Products.79d4cd71.svg"
+  },
+  {
+    "revision": "fdccebba5578ac06f7e1d01e60eba00f",
+    "url": "/FiiCodeCodLapu/static/media/Profile.fdccebba.svg"
+  },
+  {
+    "revision": "abfcbea8deeae0ae120178610acc8546",
+    "url": "/FiiCodeCodLapu/static/media/Retroholic-Demo.abfcbea8.ttf"
+  },
+  {
+    "revision": "b38c6ef4fe08325c05999ccbd0ffb773",
+    "url": "/FiiCodeCodLapu/static/media/Vintages.b38c6ef4.ttf"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/FiiCodeCodLapu/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/FiiCodeCodLapu/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/FiiCodeCodLapu/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/FiiCodeCodLapu/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/FiiCodeCodLapu/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/FiiCodeCodLapu/static/media/icons-20.cef8cdbb.woff"
+  },
+  {
+    "revision": "c2e21dd7a602f893c912ac83fe3fb8ab",
+    "url": "/FiiCodeCodLapu/static/media/retro-wallpaper-1.c2e21dd7.jpg"
+  }
+]);
